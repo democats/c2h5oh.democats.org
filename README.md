6cd865f736f7f86824a60abc06ef1435929c723e
Petar Mitchev <petar@imeleon.com>
2015-09-24 06:53:43 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# c2h5oh.democats.org
-Ethanolium pool frontend
---
+++ config.js
@@ -0,0 +1,15 @@
+var api = "http://pool.democats.org:7633";
+
+var poolHost = "pool.democats.org";
+
+var irc = "irc.freenode.net/#democats";
+
+var cryptonatorWidget = ["{symbol}-BTC"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+
+var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+
+var themeCss = "themes/ease-way-light-theme.css";
