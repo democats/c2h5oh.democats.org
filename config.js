1ba2afd0ca54196b199737c4b0d43de81fde4164
Petar Mitchev <petar@imeleon.com>
2015-11-30 09:50:22 +0200

    Blockchain explorer added

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 var api = "http://pool.democats.org:7633";
 
+var api_blockexplorer = "http://pool.democats.org:7631";
+
 var poolHost = "pool.democats.org";
 
 var irc = "irc.freenode.net/#democats";
@@ -8,8 +10,10 @@ var cryptonatorWidget = ["{symbol}-BTC"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+//var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+var blockchainExplorer = "/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+//var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var transactionExplorer = "/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/ease-way-light-theme.css";
